39d60510154a5ed871ff3331852a8f12293c2685
Elishala <elishalahansen@gmail.com>
2015-06-08 20:56:41 -0400

    the road to responsiveness has begun exclamation point

---
+++ js/onmouseover.js
@@ -0,0 +1,9 @@
+// function bigIconBookReadStreamline(x) {
+//   x.style.height = "4.9";
+//   x.style.width = "4.9";
+// }
+//
+// function normalIconBookReadStreamline(x) {
+//   x.style.height = "2.9em";
+//   x.style.width = "2.9em";
+// }
