45f66c8e8ac0e04483f423d88a00b19852deae4b
Elishala <elishalahansen@gmail.com>
2015-10-22 10:33:13 -0400

    deleted most everything from this repo, starting from scratch

--- js/onmouseover.js
+++
@@ -1,9 +0,0 @@
-// function bigIconBookReadStreamline(x) {
-//   x.style.height = "4.9";
-//   x.style.width = "4.9";
-// }
-//
-// function normalIconBookReadStreamline(x) {
-//   x.style.height = "2.9em";
-//   x.style.width = "2.9em";
-// }
